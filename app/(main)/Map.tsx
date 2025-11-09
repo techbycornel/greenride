fc2ba4c5ad5f888c8ed71bf0d8bba5a6c163108c
Authcornelius <corneliusedos@gmail.com>
2025-11-09 07:18:44 +0100

    updated

--- app/(main)/Map.tsx
+++ app/(main)/Map.tsx
@@ -1,12 +1,22 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from "react";
+import { StyleSheet, View } from "react-native";
 
 const Map = () => {
   return (
-    <View>
-      <Text>Map screen</Text>
+    <View style={styles.container}>
+      {/* <MapView style={styles.map} /> */}
     </View>
-  )
-}
+  );
+};
 
-export default Map
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
+
+export default Map;
---
+++ components/MapView.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const MapView = () => {
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+    />
+  );
+};
+
+export default MapView;
